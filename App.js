03292d904c956c5e253c7d58c582852890214c17
Aditya <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>
2025-02-03 08:07:58 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement('div', {}, [
+  React.createElement('h1', {}, 'Hello'),
+  React.createElement('h1', {}, 'Hello'),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
